133b13ca9f182654610342b44debb0904f78f2b9
unknown <basakrm988@gmail.com>
2023-03-10 16:35:38 +0530

    form and home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private router: Router
+  ){
+
+  }
+  ngOnInit(): void {
+    
+  }
+  login(){
+    this.router.navigate(['/home']);
+    console.log('ff')
+  }
 }
